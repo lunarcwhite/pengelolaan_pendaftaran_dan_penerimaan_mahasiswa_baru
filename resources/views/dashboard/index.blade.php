bd1d5797b87b863cc133ff7daef60db153581cf9
dendipratamaputrapamungkas <dendipasper33@gmail.com>
2023-01-19 21:19:35 +0700

    finishing

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -110,7 +110,7 @@
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="overview-wrap">
+                            <div class="overview-wrap" >
                                 <h2 class="title-1">Daftar Calon Mahasiswa Baru</h2>
                             </div>
                         </div>
